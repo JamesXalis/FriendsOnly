11785d90463c9b77bda0b67bff91c9c15190c4de
James Xalis <jamesxalis@icloud.com>
2022-12-02 00:22:05 -0700

    finished routes and connected them to models

--- models/User.js
+++ models/User.js
@@ -24,7 +24,10 @@ const userSchema = new Schema(
             ref: 'thought'
         }
     ],
-    friends:[ this ] //This might not work 
+    friends:[ {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    } ]
     },
     {
     toJSON: {
@@ -37,6 +40,6 @@ userSchema.virtual('friendCount').get(function(){
     return this.friends.length;
 });
 
-const User = model('User', userSchema)
+const User = model('user', userSchema)
 
 module.exports = User;
