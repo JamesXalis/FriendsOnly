9566a70f947e119f0e0330e4a58241528841a080
James Xalis <jamesxalis@icloud.com>
2022-12-01 23:44:47 -0700

    controllers and models running

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction')
+const { formatTime } = require('../utils/modelHelper')
+
+const response = new Schema({
+    thoughtText:{
+        type: String,
+        required: 'Must have text in your thought.',
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: formatTime
+    },
+    username:{
+        type: String,
+        required: 'Must have username for thought creation.'
+    },
+    reactions:[reactionSchema]
+},{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+response.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', response);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const { emailTest } = require('../utils/modelHelper')
+
+const userSchema = new Schema(
+    {
+    username:{
+        type: String,
+        required: 'Username must not be empty.',
+        unique: 'Username must be unique.',
+        trim: true
+    },
+    email: {
+        type: String,
+        required: 'Email must not be empty.',
+        unique: 'Email must be unique.',
+        validate: [emailTest, 'Please fill out a valid email.']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'thought'
+        }
+    ],
+    friends:[ this ] //This might not work 
+    },
+    {
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+    }
+);
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+const User = model('User', userSchema)
+
+module.exports = User;
